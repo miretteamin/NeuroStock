f46d6460aeca67b27a3b0deff4580a3e7b6f24e5
Mirette Moawad <miretteamin@gmail.com>
2023-06-21 20:46:34 +0300

    create graphs

---
+++ src/lstm.py
@@ -0,0 +1,20 @@
+from torch import nn
+from torch.nn import functional as F
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=1, num_layers=2,hidden_size=64, output_size=64, num_steps =10 ):
+        super().__init__()
+        self.lstm1 = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        # self.lstm2 = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, output_size)
+        self.batch_norm1 = nn.BatchNorm1d(num_steps)
+
+    def forward(self, x):
+        # h0 = torch.zeros(2, x.size(0), 100).to(device) # num_layers * num_directions, batch_size, hidden_size
+        # c0 = torch.zeros(2, x.size(0), 100).to(device)
+        x = self.batch_norm1(x)
+        out, _ = self.lstm1(x)
+        # out, _ = self.lstm2(out)
+        out = F.gelu(self.fc1(out[:, -1, :]))
+        return out
---
+++ src/week_graphs.py
@@ -0,0 +1,27 @@
+import torch
+"""
+https://pytorch-geometric.readthedocs.io/en/latest/tutorial/create_dataset.html
+"""
+from torch_geometric.data  import InMemoryDataset
+
+class WeekGraphs_Creation(InMemoryDataset):
+    def __init__(self, root, data_list, transform=None):
+        self.data_list = data_list
+        super().__init__(root, transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def processed_file_names(self):
+        return 'data.pt'
+
+    def process(self):
+        torch.save(self.collate(self.data_list), self.processed_paths[0])
+
+class WeekGraphs(InMemoryDataset):
+    def __init__(self, root, transform=None):
+        super().__init__(root, transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def processed_file_names(self):
+        return 'data.pt'
